300ff33f614b11a930153cd8f2b6334b30ca4355
Rob McQueen <robmcqn@gmail.com>
2012-02-12 17:37:08 -0500

    forgot to commit all of this. wow

---
+++ api/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls.defaults import patterns, include, url
+
+urlpatterns = patterns('project.api.views',
+  url(r'survey/$','fetch_survey'),
+  url(r'question/$','fetch_question'),
+)                       
